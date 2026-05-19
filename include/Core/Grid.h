a9f2f8ab3d308f07fd54806e3382ad2662ecab84
Andrew-8705 <abaldin05@mail.ru>
2026-05-19 23:17:30 +0300

    Add Grid class

---
+++ include/Core/Grid.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+
+struct Grid {
+    int n, m;
+    double h, k;
+    double a, b, c, d;
+    std::vector<std::vector<double>> data;
+
+    Grid(int n, int m, double a, double b, double c, double d) 
+        : n(n), m(m), a(a), b(b), c(c), d(d) 
+    {
+        h = (b - a) / n;
+        k = (d - c) / m;
+        data.resize(n + 1, std::vector<double>(m + 1, 0.0));
+    }
+};
+
+struct SolverResult {
+    Grid grid;
+    int iterations;
+    double achieved_eps;
+};
